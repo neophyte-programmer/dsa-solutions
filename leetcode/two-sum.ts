6492b0dda71bb3f63260840e11e78e5feb58375a
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-10-26 10:22:39 +0000

    first commit

---
+++ leetcode/two-sum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+    let result = []
+    for ( let i = 0; i < nums.length; i++){
+        for ( let j = i + 1; j < nums.length; j++){
+            if ( nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+    return result
+};
