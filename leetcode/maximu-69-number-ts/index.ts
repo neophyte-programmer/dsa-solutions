a7de2e1eebd48b7dbd1164ad89cc5663c2581e5e
qbentil <bentilshadrack72@gmail.com>
2022-11-07 09:33:21 +0000

    Improve solution

--- leetcode/maximu-69-number-ts/index.ts
+++ leetcode/maximu-69-number-ts/index.ts
@@ -6,11 +6,13 @@
  * Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
  */
  function maximum69Number (num: number): number {
-    const str = num.toString();
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === '6') {
-            return parseInt(str.slice(0, i) + '9' + str.slice(i + 1));
+    const arr = num.toString().split('');
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '6') {
+            arr[i] = '9';
+            break;
         }
     }
-    return num;
+    return parseInt(arr.join(''));
+
 }
